3bb98941b2f41cdc26738029972eaea639f39b6a
khun84 <khun84@gmail.com>
2017-09-14 12:18:40 +0800

    Set up show controller and view for url

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -4,4 +4,7 @@ class UrlsController < ApplicationController
     @urls = Url.all
   end
 
+  def show
+    @url = Url.find(params[:id])
+  end
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,4 +1,3 @@
 class Url < ApplicationRecord
 
-
 end
